9f9d57d086bbc34e07210c3a023734bed3724d2e
andrey372 <andrey372@gmail.com>
2020-07-24 20:11:23 -0500

    Update KNN Lab.py

--- KNN Lab.py
+++ KNN Lab.py
@@ -1,3 +1,10 @@
+import itertools
 import numpy as np
-x = np.array(1)
-print(x)
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+
+print('welcome')
