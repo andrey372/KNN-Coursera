b61a539954b56b6b5ff499f29617b5450f881df1
andrey372 <andrey372@gmail.com>
2020-07-26 17:06:32 -0500

    Final KNN

--- KNN Lab.py
+++ KNN Lab.py
@@ -1,13 +1,1 @@
-import numpy as np
-<<<<<<< HEAD
-import matplotlib.pyplot as plt
-from matplotlib.ticker import NullFormatter
-import pandas as pd
-import numpy as np
-import matplotlib.ticker as ticker
-from sklearn import
-
-=======
-x = np.array(1)
-print(x)
->>>>>>> parent of 9f9d57d... Update KNN Lab.py
+print('X')
