5a80b4c398c88115dcb25d61cf1c197cf44be3ef
andrey372 <andrey372@gmail.com>
2020-07-26 00:19:44 -0500

    v

--- KNN Lab.py
+++ KNN Lab.py
@@ -1,5 +1,5 @@
-import itertools
 import numpy as np
+<<<<<<< HEAD
 import matplotlib.pyplot as plt
 from matplotlib.ticker import NullFormatter
 import pandas as pd
@@ -7,3 +7,7 @@ import numpy as np
 import matplotlib.ticker as ticker
 from sklearn import
 
+=======
+x = np.array(1)
+print(x)
+>>>>>>> parent of 9f9d57d... Update KNN Lab.py
