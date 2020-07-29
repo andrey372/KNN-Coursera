26dc1bf22247b16850b9be052594e2aefdce2f38
andrey372 <andrey372@gmail.com>
2020-07-28 23:16:37 -0500

    final knn lab

--- KNN Lab.py
+++ KNN Lab.py
@@ -1,6 +1,7 @@
 import itertools
 import wget
 import numpy as np
+from sklearn import metrics
 import matplotlib.pyplot as plt
 from matplotlib.ticker import NullFormatter
 import pandas as pd
@@ -15,12 +16,13 @@ X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ'
 y = df['custcat'].values
 
 #Normilize data
-X = preprocessing.StandardScaler(),fit_transform(X)
+X = preprocessing.StandardScaler().fit_transform(X)
 X_train, X_test , y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
 
 #Classification
 k = 6
 neigh = KNeighborsClassifier(n_neighbors=6).fit(X_train, y_train)
 yhat_train = neigh.predict(X_train)
 yhat_test = neigh.predict(X_test)
-print(metrics.accuracy_score(y_train, yhat_train), metrics.accuracy_score(y_test, yhat_test)
+print(metrics.accuracy_score(y_train, yhat_train), metrics.accuracy_score(y_test, yhat_test))
