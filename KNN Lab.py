d20f70e62dd9522351bde8c38b5273356ee58898
Andrew <andrey372@gmail.com>
2020-08-06 13:48:28 +0000

    Update

--- KNN Lab.py
+++ KNN Lab.py
@@ -24,11 +24,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=1)
 
 #Classification
-k = 6
-neigh = KNeighborsClassifier(n_neighbors=6).fit(X_train, y_train)
+k = 4
+neigh = KNeighborsClassifier(n_neighbors=k).fit(X_train, y_train)
 yhat_train = neigh.predict(X_train)
 yhat_test = neigh.predict(X_test)
 
 print(
     metrics.accuracy_score(y_train, yhat_train),
-    metrics.accuracy_score(y_test, yhat_test))
+    metrics.accuracy_score(y_test, yhat_test),
+    metrics.classification_report(y_test, yhat_test),
+    metrics.confusion_matrix(y_test, yhat_test), neigh.score(X_test, y_test))
