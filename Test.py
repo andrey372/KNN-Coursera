9385f6cb289fae6046a715dc456677a318f6bd6e
andrey372 <andrey372@gmail.com>
2020-08-09 13:19:21 -0500

    Update

--- Test.py
+++ Test.py
@@ -1,10 +1,31 @@
 # Import libraries
 from sklearn.datasets import make_blobs
 from matplotlib import pyplot as plt
+import wget
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 
-X, y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1]], cluster_std=0.9)
-plt.scatter(X[:,0], X[:,1], marker='.')
+
+url = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/Cust_Segmentation.csv'
+df_origin = pd.read_csv(url)
+df = df_origin.drop('Address', axis = 1)
+
+#preprocc
+X = df.values[:,1:]
+X = np.nan_to_num(X)
+scaler = StandardScaler().fit_transform(X)
+
+#model
+kmeans = KMeans(init="k-means++", n_clusters=3, n_init=12)
+kmeans.fit(X)
+labels = kmeans.labels_
+
+df['clusters'] = labels
+print(df.groupby('clusters').mean())
+
+plt.scatter(X[:,0], X[:,3], c = labels, alpha=0.5)
+plt.xlabel('Age')
+plt.ylabel('Income')
 plt.show()
-
-from sklear.clister import KMeans
-
