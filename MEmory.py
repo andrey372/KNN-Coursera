7516d383208a68669e555772ea982093d00a90cb
andrey372 <andrey372@gmail.com>
2020-08-17 00:30:47 -0500

    Update

--- MEmory.py
+++ MEmory.py
@@ -2,21 +2,42 @@
 from sklearn.datasets import make_blobs
 X, y = make_blobs(n_samples = , centers = , cluster_std = )
 
-#Elbow method / KneeLocator
-from kneed import KneeLocator
-sse = []
-for k in range(1,11):
-    kmeans = KMeans(n_clusters=k , init='k-means++', n_init=8).fit(scaled_X)
-    sse.append(kmeans.inertia_)
 
-kl = KneeLocator(range(1,11), sse, curve = 'convex', direction='decreasing')
-kl.elbow
+#KMEANS MODELING
+        #Elbow method / KneeLocator
+        from kneed import KneeLocator
+        sse = []
+        for k in range(1,11):
+            kmeans = KMeans(n_clusters=k , init='k-means++', n_init=8).fit(scaled_X)
+            sse.append(kmeans.inertia_)
 
-#Modelimg K-Means
-from sklearn.cluster import KMeans
-kmeans = KMeans(init='k-means', n_clusters= , n_init= ).fit(X)
-kmeans.labels_
+        kl = KneeLocator(range(1,11), sse, curve = 'convex', direction='decreasing')
+        kl.elbow
+
+        #silhuete_coef
+        for k in range(2, 11):
+            kmeans = KMeans(n_clusters=k, init='k-means++', n_init=8),fit(X_scaled)
+            score = silhouette_score(scaled_features, kmeans.labels_)
+            ilhouette_coefficients.append(score)
+
+        #Modelimg K-Means
+        from sklearn.cluster import KMeans
+        kmeans = KMeans(init='k-means', n_clusters= , n_init= ).fit(X)
+        kmeans.labels_
+
+#AGGLOMERATIVE CLUSTERING
+        #dendogram
+        dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
+        plt.show()
+
+        #Agglomerative model
+        from sklearn.cluster import AgglomerativeClustering
+        cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward').fit(X_scaled)
 
 
 #pd
 df.apply(pd.to_numeric, errors='coerce')
+
+#sns labeleb scattering
+facet = sns.lmplot(data=df, x='X', y='y', hue='clusters',
+                   fit_reg=False)
--- Test.py
+++ Test.py
@@ -6,26 +6,18 @@ import numpy as np
 import pandas as pd
 from kneed import KneeLocator
 import matplotlib.pyplot as plt
+import matplotlib
 from sklearn.datasets import load_digits
 import scipy.cluster.hierarchy as shc
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import adjusted_rand_score
+import seaborn as sns
 
 
-#blobs
+employee_numbers = [2, 9, 18, 28]
+employee_names = ["Candice", "Ava", "Andrew", "Lucas"]
 
-X, y  = make_blobs(n_samples=500, centers=5, cluster_std=0.9)
 
-#dendogram
-dendogram = shc.dendrogram(shc.linkage(X, method='ward'))
-plt.show()
+table = pd.DataFrame(employee_names, employee_numbers).reset_index()
 
-#model
-cluster = AgglomerativeClustering(n_clusters=4, linkage='ward').fit(X)
-colors = cluster.labels_
-
-plt.scatter(X[:,0], X[:,1], c = X[:,0])
-plt.show()
-
-agglomerativew
-agglomerative
-agglomerative
+print(table)
